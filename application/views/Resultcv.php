111c8accb96632d3200a7e2114ca5b06927aa88f
Yusril <erilmbamba@gmail.com>
2022-12-21 12:47:28 +0700

    Small Revision

--- application/views/Resultcv.php
+++ application/views/Resultcv.php
@@ -90,7 +90,7 @@
           </div>
           <ul>
             <li>
-              <p>detail1</p>
+              <p><?= $edu->iname; ?></p>
               <p><?= $edu->otheredu ?></p>
             </li>
           </ul>
@@ -119,6 +119,9 @@
       </div>
       <!-- end for rest of basic -->
   </div>
+  <script>
+    window.print();
+  </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
